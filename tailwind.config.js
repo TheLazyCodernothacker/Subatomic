89dafdf7a213a9e50fe38676a9ef4ede08226b9e
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2024-01-03 05:38:47 +0000

    killed css :(

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/pages/**/*.{js,mjs,ts,tsx,jsx}", "./app/components/**/*.{js,mjs,ts,tsx,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
